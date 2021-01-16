573985e786d1b11acc40230e9bb08505ff49512c
unknown <bryanlin107@gmail.com>
2021-01-16 17:31:47 -0500

    Change init store state to dummydata

--- src/store/reducers/input-reducer.ts
+++ src/store/reducers/input-reducer.ts
@@ -1,13 +1,108 @@
-
-interface InitState{
-
-}
+import {Input, Teams, Heros} from '../../types';
 
 interface ActionType{
     type: string;
 }
 
-const inputReducer = (state:InitState={}, action:ActionType) =>{
+const dummyData:Input = {
+    winner: undefined,
+    home:{
+        team: Teams.dynasty,
+        players:[
+            {
+                hero: Heros.symmetra,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 1",
+            },
+            {
+                hero: Heros.torb,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 2",
+            },
+            {
+                hero: Heros.tracer,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 3",
+            },
+            {
+                hero: Heros.widowmaker,
+                ultCharge: 50,
+                isAlive: false,
+                health: 100,
+                username:"player 4",
+            },
+            {
+                hero: Heros.winston,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 5",
+            },
+            {
+                hero: Heros.zarya,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 6",
+            },
+        ]
+    },
+    away:{
+        team: Teams.dynasty,
+        players:[
+            {
+                hero: Heros.zenyatta,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 1",
+            },
+            {
+                hero: Heros.zarya,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 2",
+            },
+            {
+                hero: Heros.reaper,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 3",
+            },
+            {
+                hero: Heros.reinhardt,
+                ultCharge: 50,
+                isAlive: false,
+                health: 100,
+                username:"player 4",
+            },
+            {
+                hero: Heros.ana,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 5",
+            },
+            {
+                hero: Heros.mccree,
+                ultCharge: 50,
+                isAlive: true,
+                health: 100,
+                username:"player 6",
+            },
+        ]
+    }
+}
+
+const inputReducer = (state:Input=dummyData, action:ActionType) =>{
     switch(action.type){
         default:
             return state;
